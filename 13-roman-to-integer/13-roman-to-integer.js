9e5fcb3c4a976ac982754eb2558929624e44aac0
Vitor <92526735+vsvsilva@users.noreply.github.com>
2022-05-29 00:01:41 -0300

    Time: 198 ms (41.54%), Space: 46.4 MB (92.04%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+
+    const roman_hash = { 
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+
+    for (i=0; i < s.length; i++){
+        const cur = roman_hash[s[i]];
+        const next = roman_hash[s[i+1]];
+
+        if (cur < next){
+            result += next - cur;
+            i++;
+        } else {
+            result += cur;
+        }
+    };
+
+    return result; 
+};
