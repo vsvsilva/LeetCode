e4fa899fda739dfcfa770b63784af75ab7d3f3f6
Vitor <92526735+vsvsilva@users.noreply.github.com>
2022-06-15 11:24:11 -0300

    Time: 130 ms (5.38%), Space: 41.8 MB (88.93%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var searchInsert = function(nums, target) {
+    let start = 0;
+    let end = nums.length-1;
+   
+    while(start <= end){
+        let mid = Math.floor((start+end)/2);
+        
+        if(nums[mid] === target){
+            return mid;
+        }else if(target < nums[mid]){
+            end = mid - 1;
+        }else{
+            start = mid + 1;
+        }
+    }
+    return start;
+};
